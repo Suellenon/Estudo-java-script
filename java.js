759dd79d28b77a99bf79fe86e4e74beefcee5b03
suellenon <sullen502@gmail.com>
2024-07-01 23:32:12 -0300

    respondi7

---
+++ java.js
@@ -0,0 +1,109 @@
+// 1 QUESTAO
+let nome = "Suellen"
+console.log(nome)
+
+// 2 questao
+let comidas=['arroz', 'feijão', 'macarrao','carne'];
+console.log(comidas)
+
+// 3 questao
+console.log ( comidas[3])
+
+//4 questao
+
+
+
+
+//5 questao
+let A = 2
+let B = 4
+let C = 1
+let resultado= A + B
+console.log(resultado)
+console.log(resultado < C)
+
+// 6 questao
+var numero = "5"
+var soma = numero + 10;
+console.log(soma);
+
+   //é uma string , concatenou 
+   var numero = "5"
+   var soma = parseInt(numero) + 10;
+   console.log(soma)
+
+
+// 7 questao
+
+
+
+
+
+
+
+// 8 questao
+
+
+
+
+
+
+//9 questao
+let n= 40
+console.log(`Seu antecessor é ${n-1} e seu sucessor é ${n+1}`)
+
+//10questao
+
+
+
+
+//11 questao
+
+//12 questao
+
+
+    
+
+    
+
+
+
+//13 questao
+
+
+
+
+   
+
+
+//14 questao
+
+
+
+// 15 questao
+
+
+//16 questao
+
+
+//17 questao
+
+
+// 18 questao
+
+
+//19 questao 
+
+
+
+
+
+// questao20
+
+
+
+//questao 21
+var frutas = [ "maçã", "banana","laranja"];
+console.log(frutas[3]);
+console.log(frutas[2])
+//não tem fruta na posição 3
